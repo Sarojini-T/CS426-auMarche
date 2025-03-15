2ad3466a455f889a16e9713b5a2bfaee4b6a674e
dannycedrone <99098617+dannycedrone@users.noreply.github.com>
2025-03-15 00:02:55 -0400

    include item data in location data

--- src/location-data.ts
+++ src/location-data.ts
@@ -1,32 +1,40 @@
+import { ItemData, ITEM_DATA } from "./item-data";
+
 type LocationData = {
     name: string;
-    //ingredients: IngredientData[]
-    city: string
+    items: ItemData[];
+    city: string;
 }
 
 const LOCATION_DATA: LocationData[] = [
     {
         name: "Big Y",
+        items: [ITEM_DATA[3]],
         city: "Amherst"
     },
     {
         name: "Mum's House",
+        items: [ITEM_DATA[1], ITEM_DATA[2]],
         city: "Amherst"
     },
     {
         name: "Stop & Shop",
+        items: [ITEM_DATA[0], ITEM_DATA[2]],
         city: "Amherst"
     },
     {
         name: "Big Y",
+        items: [ITEM_DATA[2], ITEM_DATA[4], ITEM_DATA[5]],
         city: "Amherst"
     },
     {
         name: "Don Quijote Market",
+        items: [ITEM_DATA[0], ITEM_DATA[2]],
         city: "Amherst"
     },
     {
         name: "Nubian Markets",
+        items: [ITEM_DATA[2], ITEM_DATA[4], ITEM_DATA[5]],
         city: "Amherst"
     }
 ]
