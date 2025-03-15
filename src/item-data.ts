51bd0c984f33bf0a6a5b32e2ef98cca84809f66f
Sarojini Torchon <sarojini.torchon@gmail.com>
2025-03-14 20:46:29 -0400

    adding item data

---
+++ src/item-data.ts
@@ -0,0 +1,44 @@
+import { LocationData, LOCATION_DATA } from "./location-data";
+
+type ItemData = {
+    englishNames :string | string[],
+    haitianKreyolNames : string | string[]
+    japaneseNames : string | string[]
+    locations : LocationData[]
+}
+
+const ITEM_DATA : ItemData[] = [
+    {
+        englishNames : ["Taro", "Malanga Root"], 
+        haitianKreyolNames : "Malanga",
+        japaneseNames : ["Maranga no ne","マランガの根"],
+        locations : [LOCATION_DATA[2],LOCATION_DATA[4]]
+    },
+    {
+        englishNames : "Chayote", 
+        haitianKreyolNames : "Militon",
+        japaneseNames : ["Hayatōri", "ハヤトウリ"],
+        locations : [LOCATION_DATA[1]]
+    },
+    {
+        englishNames : "Bean Sprouts", 
+        haitianKreyolNames : "Jèrm Pwa",
+        japaneseNames : ["Moyashi","もやし"],
+        locations : [LOCATION_DATA[1],LOCATION_DATA[2],LOCATION_DATA[3],LOCATION_DATA[4], LOCATION_DATA[5]]
+    },
+    {
+        englishNames : ["Kikurage", "Wood Ear Mushroom", "Black Fungus"], 
+        haitianKreyolNames : "Zòrèy Bwa Djondjon",
+        japaneseNames : ["Kikurage", "木耳"],
+        locations : [LOCATION_DATA[0]]
+    },
+    {
+        englishNames : "Dashi Powder", 
+        haitianKreyolNames : "Poud Dashi",
+        japaneseNames : ["Dashi-ko","だし粉"],
+        locations : [LOCATION_DATA[3], LOCATION_DATA[5]]
+    },
+]
+
+export { ITEM_DATA }
+export type { ItemData }
