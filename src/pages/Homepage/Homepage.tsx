cdc0556005fad7ee27d531833c8b513d6f4dfb85
Sarojini Torchon <sarojini.torchon@gmail.com>
2025-03-28 14:32:07 -0400

    Added background image to homepage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,14 +1,16 @@
+import homepageImg from "./assets/homepageImg.jpeg";
+
 const Homepage = () => {
     return(
       <>
         {/* Grid container for homepage that will contain the navbar on top,
         Image and website description side by side below it and a footer at
         the bottom */}
-        <div className="grid">
-            <div className="border col-span-10">Navbar </div>
-            <div className="border col-start-1 col-span-6">Image </div>
-            <div className="border col-start-7 col-span-4">Description</div>
-            <div className="border col-start-1 col-span-10">Footer</div>
+        <div className="grid grid-cols-2 grid-rows-3 h-full">
+            <div className="border col-span-2">Navbar </div>
+            <img src={homepageImg} />
+            <div className="border">Description</div>
+            <div className="border col-span-2">Footer</div>
         </div> 
       </>
     );
