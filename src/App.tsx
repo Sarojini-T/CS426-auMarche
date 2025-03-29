c52c2798376535b83963d2a9ddd9e51068bc16aa
dannycedrone <99098617+dannycedrone@users.noreply.github.com>
2025-03-29 11:32:00 -0400

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router'
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="font-jomhuria text-5xl">Hi! Testing Jomhuria Font</h1>
+    <Router>
+      <div className='App'>
+        <div className='content'>
+          <Routes>
+            <Route path='/'>
+              {/* <Route index element={ <HomePage /> } /> */}
+            </Route>
+            <Route path='/suggestion'>
+              {/* <Route index element={ <SuggestionPage /> } /> */}
+            </Route>
+            <Route path='/profile'>
+              {/* <Route index element={ <ProfilePage /> } /> */}
+            </Route>
+            <Route path='/results'>
+              {/* <Route index element={ <ResultsPage /> } /> */}
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </>
+    </Router>
   );
 }
 
