9a4b445a5237e531c8de62148437991b546d912c
Sarah Jang <ssjang@umass.edu>
2025-03-29 19:50:30 -0400

    Pull main changes and update data mapping for items and places

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,22 +1,41 @@
 import Title from "./components/Title";
-import { USER_DATA } from "../../data/user-data";
+import ProfilePic from "./assets/avatar.png";
+import { ITEM_DATA } from "../../data/item-data";
+import { LOCATION_DATA } from "../../data/location-data";
 import ListItem from "./components/ListItem";
 
 const ProfilePage = () => {
   return (
     <div className="w-[100vw] h-[100vh]">
-      <div className="h-[70%]"></div>
-      <div className="flex flex-row">
-        <div className="flex flex-col">
+      <div className="h-[70%] flex flex-col">
+        <h1 className="font-jomhuria text-4xl">Profile</h1>
+        <div className="flex flex-row justify-center">
+          <div>
+            <img src={ProfilePic} />
+          </div>
+          <div className="flex flex-col font-jomhuria">
+            <div>
+              <h1 className="">John Doe</h1>
+              <button></button>
+            </div>
+            <div>
+              <h1>Boston, MA</h1>
+              <button></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row w-[100%]">
+        <div className="flex flex-col justify-evenly">
           <Title text="Saved Ingredients" />
-          {USER_DATA[0].ingredients.map((ingredient, index) => (
-            <ListItem key={index} text={ingredient} />
+          {ITEM_DATA.map((item, index) => (
+            <ListItem text={item.haitianKreyolNames[0]} key={index} />
           ))}
         </div>
         <div className="flex flex-col">
           <Title text="Saved Locations" />
-          {USER_DATA[0].locations.map((location, index) => (
-            <ListItem key={index} text={location.name} />
+          {LOCATION_DATA.map((location, index) => (
+            <ListItem text={location.name} key={index} />
           ))}
         </div>
       </div>
